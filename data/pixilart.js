b55c3954470435776629ad173a85008edfc42cc0
fish0119freelancer <timmyjuan0119@gmail.com>
2026-07-31 12:22:49 +0800

    chore: update site content

--- data/pixilart.js
+++ data/pixilart.js
@@ -1,17 +1,26 @@
 export const pixilartItems = [
   {
+    "image": "/images/content/dbf12b20e7ba8040105592818698835cedf83e62ad7b27e3d288fef20526b334.gif",
+    "slug": "untitled-19",
+    "title": "",
+    "caption": "Succubus Successor: Delilah's Juicy Journey\n魅魔繼承者：黛莉菈的淫欲冒險/Characters",
+    "href": "",
+    "category": "Characters",
+    "breakBefore": false
+  },
+  {
     "image": "/images/content/988266e49f4c2aea32cff0d0b1be700269043fc5ec8fab897231a12f2faa9b2a.gif",
     "slug": "untitled-4",
-    "title": "Storm edge/角色Character",
-    "caption": "",
+    "title": "",
+    "caption": "Storm edge風暴馭使/Character",
     "href": "",
     "category": "Characters"
   },
   {
     "image": "/images/content/06da95e8e756df091675f2d04887541d676f328f276102a45c781ab319ec5930.gif",
     "slug": "untitled-3",
-    "title": "Storm edge/角色Character",
-    "caption": "",
+    "title": "",
+    "caption": "Storm edge風暴馭使/Character",
     "href": "",
     "category": "Characters"
   },
@@ -19,7 +28,7 @@ export const pixilartItems = [
     "image": "/images/content/956b35167b94601230964dad847e00a091dceb14ea301c7c4f95da50a1948c20.gif",
     "slug": "untitled-7",
     "title": "",
-    "caption": "Storm edge/角色Character",
+    "caption": "Storm edge風暴馭使/Character",
     "href": "",
     "category": "Characters",
     "breakBefore": false
@@ -28,7 +37,7 @@ export const pixilartItems = [
     "image": "/images/content/7ca70f28fad61551726fde67fc2d6a5a13cf2d6dc209784ba770f4bc952d062e.gif",
     "slug": "untitled-10",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Items",
     "href": "",
     "category": "Items",
     "breakBefore": false
@@ -37,7 +46,7 @@ export const pixilartItems = [
     "image": "/images/content/b0220dc044f5c21aa07359d26cda28872938318271d071a41a3af94940d25fb8.gif",
     "slug": "Storm-edge門animation",
     "title": "Storm edge/門animation",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Items",
     "href": "",
     "category": "Items",
     "breakBefore": false
@@ -46,7 +55,7 @@ export const pixilartItems = [
     "image": "/images/content/5474d585c961c368a52755cd680084f6b81cc03b883cbf0b3f3c7d5e307c1d34.gif",
     "slug": "untitled-9",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Items",
     "href": "",
     "category": "Items",
     "breakBefore": false
@@ -55,7 +64,7 @@ export const pixilartItems = [
     "image": "/images/content/191a6219911c36177eb6bf7fb5086e9cebd3980985a1bebbcdbb063d4858517b.gif",
     "slug": "untitled-8",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Items",
     "href": "",
     "category": "Items",
     "breakBefore": false
@@ -64,7 +73,7 @@ export const pixilartItems = [
     "image": "/images/content/5fb0ea434f15af01f64d6538090cfc33ecfd2f442610f0763917ea327453d4e4.png",
     "slug": "untitled-18",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Map",
     "href": "",
     "category": "Map",
     "breakBefore": false
@@ -73,7 +82,7 @@ export const pixilartItems = [
     "image": "/images/content/111567cc471b86380bcf2831b872d320162c899891916e997df173f2229b0b31.png",
     "slug": "untitled-17",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Map",
     "href": "",
     "category": "Map",
     "breakBefore": false
@@ -82,7 +91,7 @@ export const pixilartItems = [
     "image": "/images/content/09a0ec1d0cae32a4e76502003d0e5aeca5a75707f36aeed321754249af2c1151.png",
     "slug": "untitled-16",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/Map",
     "href": "",
     "category": "Map",
     "breakBefore": false
@@ -91,7 +100,7 @@ export const pixilartItems = [
     "image": "/images/content/c19390b591854737f5a6ae60ce7e695dc741d02f1dbf973ba2d18f91391ea161.png",
     "slug": "untitled-2",
     "title": "",
-    "caption": "Storm edge/道具icon",
+    "caption": "Storm edge風暴馭使/Items",
     "href": "",
     "category": "Items",
     "breakBefore": false
@@ -100,7 +109,7 @@ export const pixilartItems = [
     "image": "/images/content/78ddb2a675517a4659ace534d45b832e18ef0d80cfd393a5537ef02fe8e038ec.png",
     "slug": "untitled-5",
     "title": "",
-    "caption": "Storm edge/道具icon",
+    "caption": "Storm edge風暴馭使/Items",
     "href": "",
     "category": "Items",
     "breakBefore": false
@@ -109,7 +118,7 @@ export const pixilartItems = [
     "image": "/images/content/f70cb4cc283a23cf53cc402a9e8c3c4335527c55452a4833b0262e42eb9f160b.jpg",
     "slug": "untitled-13",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/UI",
     "href": "",
     "category": "UI",
     "breakBefore": false
@@ -118,7 +127,7 @@ export const pixilartItems = [
     "image": "/images/content/f31bee878136224bd47691f41708d2ebc826cdce7059158df2a2cc4fd0d0681c.png",
     "slug": "untitled-11",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/UI",
     "href": "",
     "category": "UI",
     "breakBefore": false
@@ -127,7 +136,7 @@ export const pixilartItems = [
     "image": "/images/content/c369d4c79cdb30046692630c78a3bf2431c781c7552beaf03dd5288410d119fc.gif",
     "slug": "untitled-12",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/UI",
     "href": "",
     "category": "UI",
     "breakBefore": false
@@ -136,7 +145,7 @@ export const pixilartItems = [
     "image": "/images/content/ca4f51d566b1cb5aa9efc37d65115fa02b98667378b52a09609cdb8baa3b15cc.png",
     "slug": "untitled-14",
     "title": "",
-    "caption": "",
+    "caption": "Storm edge風暴馭使/UI",
     "href": "",
     "category": "UI",
     "breakBefore": false
@@ -145,7 +154,7 @@ export const pixilartItems = [
     "image": "/images/content/ada7673de17586018d09e60c091a41f40ee4c749c4053afe2c491a90891d1bc1.gif",
     "slug": "untitled-6",
     "title": "",
-    "caption": "Storm edge/寶石動畫animation",
+    "caption": "Storm edge風暴馭使/UI ａnimation",
     "href": "",
     "category": "UI"
   },
